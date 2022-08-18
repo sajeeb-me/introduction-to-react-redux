cbf1c4bde24186b2e985f0d74e502e8243f3c1ce
sdshuvo98 <hellosdshuvo@gamil.com>
2022-08-18 13:47:27 +0600

    combineReducers

--- src/redux/counter/counterReducer.js
+++ src/redux/counter/counterReducer.js
@@ -1,7 +1,7 @@
 import { DECREMENT, INCREMENT } from "./actionTypes";
 
 const initialState = {
-    value: 0,
+    value: 20,
 };
 
 const counterReducer = (state = initialState, action) => {
@@ -9,12 +9,12 @@ const counterReducer = (state = initialState, action) => {
         case INCREMENT:
             return {
                 ...state,
-                value: state.value + action.payload,
+                value: state.value + 1,
             };
         case DECREMENT:
             return {
                 ...state,
-                value: state.value - action.payload,
+                value: state.value - 1,
             };
         default:
             return state;
